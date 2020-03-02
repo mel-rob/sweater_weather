0b65bf0f297681031842aae91f37b68df1587176
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-02 11:37:33 -0700

    Refactor test for munchie request

--- spec/endpoints/munchie_spec.rb
+++ spec/endpoints/munchie_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'I can receive a munchie for a trip', :type => :request do
-  it 'returns all necessary information based on spec' do
+  it 'returns all necessary information based on spec', :vcr do
 
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese', headers: {
       'Content-Type' => 'application/json',
@@ -15,6 +15,7 @@ RSpec.describe 'I can receive a munchie for a trip', :type => :request do
     expect(munchie[:end_location]).to be_a String
     expect(munchie[:travel_time]).to be_a String
     expect(munchie[:forecast]).to be_a String
+    expect(munchie[:restaurant].length).to eq(2)
     expect(munchie[:restaurant].keys).to eq([:name, :address])
   end
 end
