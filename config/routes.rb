b9920f84994286c911bb3c00842957317ae093a1
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-02 09:42:24 -0700

    Add get '/munchies' to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
       get '/backgrounds', to: 'backgrounds#index'
+      get '/munchies', to: 'munchies#show'
     end
   end
 end
