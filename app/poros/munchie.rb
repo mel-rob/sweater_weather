2283b61cd9950cd17719520f784a2191e44dfd10
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-02 10:02:45 -0700

    Add end_location and travel_time to Munchie poro

--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -7,4 +7,19 @@ class Munchie
     @destination = destination
     @food_type = food_type
   end
+
+  def end_location
+    google_direction_data[:end_address]
+  end
+
+  def travel_time
+    google_direction_data[:duration][:text]
+  end
+
+
+private
+
+  def google_direction_data
+    @google_direction_data ||= GoogleService.new(@origin, @destination).get_directions[:routes][0][:legs][0]
+  end
 end
