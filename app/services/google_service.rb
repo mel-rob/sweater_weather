7c06943fbf96c37cf37dd91ce0c661eccd5f05d8
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-03 11:17:48 -0700

    Add get_directions method to Google API service

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,26 +1,38 @@
 class GoogleService
 
-  def initialize(location)
-    @location = location
+  def initialize(origin)
+    @origin = origin
   end
 
   def get_location
     location_data
   end
 
+  def get_directions(destination)
+    direction_data(destination)
+  end
+
 private
 
-  def conn
-    Faraday.new('https://maps.googleapis.com/maps/api/geocode/json') do |faraday|
+  def conn(type)
+    Faraday.new("https://maps.googleapis.com/maps/api/#{type}/json") do |faraday|
       faraday.params['key'] = ENV['GOOGLE_API_KEY']
       faraday.adapter Faraday.default_adapter
     end
   end
 
   def location_data
-    response = conn.get do |request|
-      request.params['address'] = @location
+    response = conn('geocode').get do |request|
+      request.params['address'] = @origin
     end
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def direction_data(destination)
+    response = conn('directions').get do |request|
+      request.params['origin'] = @origin
+      request.params['destination'] = destination
+    end
+    JSON.parse(response.body), symbolize_names: true)
   end
 end
