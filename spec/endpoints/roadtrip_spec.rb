32b610309ade488c8de39de242486afc4df225ec
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-03 11:36:23 -0700

    Refactor test for road trip request

--- spec/endpoints/roadtrip_spec.rb
+++ spec/endpoints/roadtrip_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 describe 'I can create a road trip', :type => :request do
 
   it 'can take in road trip info and return travel time and arrival forecast', :vcr do
-    WebMock.enable_net_connect!
-    VCR.eject_cassette
-    VCR.turn_off!(ignore_cassettes: true)
     user = User.create({
       email: "test@example.com",
       password: "password",
@@ -22,12 +19,13 @@ describe 'I can create a road trip', :type => :request do
 
     expect(response).to be_successful
     road_trip = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
     expect(road_trip[:origin]).to eq('Denver,CO')
     expect(road_trip[:destination]).to eq('Pueblo,CO')
     expect(road_trip[:travel_time]).to eq('1 hour 48 mins')
-    expect(road_trip[:forecast].keys).to eq([:summary, :temp])
-    expect(road_trip[:forecast][:temp]).to be_a Float
-    expect(road_trip[:forecast][:summary]).to be_a String
+    expect(road_trip[:arrival_forecast].keys).to eq([:summary, :temp])
+    expect(road_trip[:arrival_forecast][:temp]).to be_a Float
+    expect(road_trip[:arrival_forecast][:summary]).to be_a String
   end
 
   it 'will not take in road trip info without a valid API key' do
@@ -41,6 +39,7 @@ describe 'I can create a road trip', :type => :request do
 
     expect(response).to_not be_successful
     road_trip = JSON.parse(response.body, symbolize_names: true)
+
     expect(road_trip[:failed]).to eq('Unauthorized')
   end
 end
