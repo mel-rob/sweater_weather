528d29958d68a3508be61a580ba5a66aaf80ca7c
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-03 15:34:12 -0700

    Refactor Today poro

--- app/poros/today.rb
+++ app/poros/today.rb
@@ -1,18 +1,16 @@
 class Today
 
-  def self.info(forecast_data)
-    {
-      today_summary: forecast_data[:hourly][:summary],
-      tonight_summary: self.tonight(forecast_data[:hourly][:data]),
-      high: forecast_data[:daily][:data][0][:temperatureHigh],
-      low: forecast_data[:daily][:data][0][:temperatureLow],
-      date: Time.now.to_date
-    }
+  def initialize(forecast_data)
+    @today_summary = forecast_data[:hourly][:summary],
+    @tonight_summary = tonight(forecast_data[:hourly][:data]),
+    @high = forecast_data[:daily][:data][0][:temperatureHigh],
+    @low = forecast_data[:daily][:data][0][:temperatureLow],
+    @date = Time.now.to_date
   end
 
 private
 
-  def self.tonight(hour_data)
+  def tonight(hour_data)
     hour_20 = hour_data.find do |hour|
       Time.at(hour[:time]).in_time_zone('UTC').hour == 20
     end
