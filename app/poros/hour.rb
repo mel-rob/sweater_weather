aaf2abd37d724e0f6e8f3fdd6f1ecd6eca32e6c0
Melissa Robbins <melissamrobbins@gmail.com>
2020-03-03 15:34:06 -0700

    Refactor Hour poro

--- app/poros/hour.rb
+++ app/poros/hour.rb
@@ -1,9 +1,8 @@
 class Hour
-  def self.info(hour_info)
-    {
-      time: hour_info[:time],
-      temp: hour_info[:temperature],
-      icon: hour_info[:icon]
-    }
+
+  def initialize(hour_info)
+    @time = hour_info[:time],
+    @temp = hour_info[:temperature],
+    @icon = hour_info[:icon]
   end
 end
